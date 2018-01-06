1b6721566c9ff418a4c75d00cc318163ed0dba99
Zach Surgett <zsurgett@gmail.com>
2018-01-06 14:08:35 -0700

    Added a table and appending items to it
    I've added a table to hold the steps and results of the algorithm. You can now select a number between 1 and 15000 and when you click the test button, the number will be put through the test and the results are then appended to the table. To ensure that the table is cleared between test, all children elements of the table are removed when the button is clicked.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,42 @@
 var count = 0;
+var submitButton = $("#submit");
+var tableBody = $(".tbody");
 
 function Collatz (number) {
-console.log(number);
-count += 1;
-  if (number > 1) {
-    if (number % 2 === 0) {
-      number /= 2;
-      Collatz(number);
+  'use strict';
+  $(submitButton).addClass("disabled");
+  console.log(number);
+  count += 1;
+
+  $(tableBody).append("<tr class=\"tableRow\"><td>" + count + "</td><td>" + number + "</td></tr>");
+
+    if (number > 1) {
+      if (number % 2 === 0) {
+        number /= 2;
+        Collatz(number);
+      }
+      else if (number % 2 !== 0) {
+        number = (number * 3) + 1;
+        Collatz(number);
+      }
+      else {
+        console.error("ERROR. STOP FDSJKDLSJFKSDLJF");
+      }
     }
-    else if (number % 2 !== 0) {
-      number = (number * 3) + 1;
-      Collatz(number);
+    else if (number === 1) {
+      console.log("Completed. It took " + count + " calls to the function to get to 1.")
     }
     else {
       console.error("ERROR. STOP FDSJKDLSJFKSDLJF");
     }
-  }
-  else if (number === 1) {
-    console.log("Completed. It took " + count + " calls to the function to get to 1.")
-  }
-  else if (number < 1){
-    console.error("The number must be greater than one");
-  }
-  else {
-    console.error("ERROR. STOP FDSJKDLSJFKSDLJF");
-  }
+
+
   count = 0;
+  $(submitButton).removeClass("disabled");
 }
 
-var testNum = document.getElementById("number-to-test").value;
-
-console.log(testNum);
-
-Collatz(testNum);
+$(submitButton).click(function() {
+  var testNum = $("#number-to-test").val();
+  $(tableBody).children().remove(".tableRow");
+  Collatz(testNum);
+});
